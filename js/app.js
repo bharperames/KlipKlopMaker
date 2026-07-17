600b7c09c17e4c5fc3eff490ba80636511d2ce40
Brett Harper <brett.harper@gmail.com>
2026-07-17 12:57:43 -0500

    feat: keep larger elevator ascent and remove grand circuit to maintain exactly 8 standard scenes

--- js/app.js
+++ js/app.js
@@ -871,7 +871,7 @@ if (sceneGrid && btnScenePicker && sceneGridDropdown) {
 
     const sceneFiles = [
         '01-first-ramp', '02-demo-tower', '03-grand-helix',
-        '09-switchyard', '10-lift-and-return', '11-palm-resort', '12-perpetual-motion', '13-grand-circuit'
+        '09-switchyard', '10-lift-and-return', '11-palm-resort', '12-perpetual-motion', '15-elevator-ascent'
     ];
 
     Promise.all(sceneFiles.map(async (filename) => {
