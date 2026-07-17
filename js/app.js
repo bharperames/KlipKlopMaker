6f88401181fee17fc3fdffde96008341381939d1
Brett Harper <brett.harper@gmail.com>
2026-07-17 12:47:22 -0500

    clean: remove s-curve meadow and elevator showcase tracks, reducing selection menu to 8 standard options

--- js/app.js
+++ js/app.js
@@ -870,9 +870,8 @@ if (sceneGrid && btnScenePicker && sceneGridDropdown) {
     }
 
     const sceneFiles = [
-        '01-first-ramp', '02-demo-tower', '03-grand-helix', '04-s-curve-meadow',
-        '09-switchyard', '10-lift-and-return', '11-palm-resort', '12-perpetual-motion', '13-grand-circuit',
-        '14-elevator-showcase', '15-elevator-ascent'
+        '01-first-ramp', '02-demo-tower', '03-grand-helix',
+        '09-switchyard', '10-lift-and-return', '11-palm-resort', '12-perpetual-motion', '13-grand-circuit'
     ];
 
     Promise.all(sceneFiles.map(async (filename) => {
